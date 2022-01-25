73c4e65f47bcfdd4c38d24c407a31e52b8cef1ec
Surya Venkatesan <suryaedu71@gmail.com>
2022-01-25 12:31:04 +0530

    states intro

--- src/components/Holidays.js
+++ src/components/Holidays.js
@@ -1,12 +1,21 @@
 import React, {Component} from "react";
 
-
 // Class component
 class Holidays extends Component{
-    render(){
-        return <h2>The holiday has begun, you have {this.props.number} days</h2>
+    constructor(){
+        super()
+        this.state = {
+            start_date : "20 Feb",
+            end_date : "28 Feb",
+            pending_days :8
+        }
     }
+    render(){
+        return <h4>The holiday has begun, you have {this.props.number} days,
+        Starting from {this.state.start_date} till {this.state.end_date}.
+        Pending {this.state.pending_days} are left</h4>
+    }
+    
 }
-
 
 export default Holidays
