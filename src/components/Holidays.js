8256413334d812b6ce17b774a6811c97cbbfbe0f
Surya Venkatesan <suryaedu71@gmail.com>
2022-01-25 11:17:17 +0530

    Defined components creation

---
+++ src/components/Holidays.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+
+// Class component
+class Holidays extends Component{
+    render(){
+        return <h2>The holiday has begun</h2>
+    }
+}
+
+
+export default Holidays
---
+++ src/components/Semester.js
@@ -0,0 +1,8 @@
+import React,{Component} from "react";
+
+// Functioal Component
+function Semester(){
+    return <h1>The winter has begin</h1>
+}
+
+export default Semester
