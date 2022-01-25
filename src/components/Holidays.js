bdcfadf77deeb643ecdb18fb9896647d2181a21b
Surya Venkatesan <suryaedu71@gmail.com>
2022-01-25 12:44:47 +0530

    Conditionals begin

--- src/components/Holidays.js
+++ src/components/Holidays.js
@@ -1,21 +1,35 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 // Class component
-class Holidays extends Component{
-    constructor(){
+class Holidays extends Component {
+    constructor() {
         super()
         this.state = {
-            start_date : "20 Feb",
-            end_date : "28 Feb",
-            pending_days :8
+            start_date: "20 Feb",
+            end_date: "28 Feb",
+            pending_days: 0 // change it to 8 to execute the else statement
         }
     }
-    render(){
-        return <h4>The holiday has begun, you have {this.props.number} days,
-        Starting from {this.state.start_date} till {this.state.end_date}.
-        Pending {this.state.pending_days} are left</h4>
+
+    changePendingDate() {
+        this.state.pending_days -= 1
+        alert(this.state.pending_days)
     }
-    
+
+    render() {
+        if (this.state.pending_days < 1) {
+            return <div>
+                <h4>Your holiday has ended.. Sed life vro</h4>
+            </div>
+        } else {
+            return <div>
+                <h4>The holiday has begun, you have {this.props.number} days,
+                    Starting from {this.state.start_date} till {this.state.end_date}.
+                    Pending {this.state.pending_days} are left</h4>
+                <button onClick={() => this.changePendingDate()}>Reduce pending days</button>
+            </div>
+        }
+    }
 }
 
 export default Holidays
--- src/components/Semester.js
+++ src/components/Semester.js
@@ -1,8 +1,15 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
 
 // Functioal Component
-function Semester(props){
-    return <h1>Your exams starts by {props.month}</h1>
+function Semester(props) {
+
+    const exam = (subject_name,event) => {
+        alert(event.type + subject_name)
+    }
+    return <div>
+        <h1>Your exams starts by {props.month}</h1>
+        <button onClick={(event) => exam("POAI",event)}>Reduce pending days</button>
+    </div>
 }
 
 export default Semester
